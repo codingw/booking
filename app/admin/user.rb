b60865a582748eb6506871a581107526543cf110
yudapc <yuda.pc@gmail.com>
2013-12-27 11:31:27 +0000

    add active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
 
-  
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -15,19 +15,24 @@ ActiveAdmin.register User do
   # end
   #
 
-  permit_params :username  
+  filter :username
+  filter :email
+
+
+  permit_params :username
 
   scope :active
   scope :inactive
-  
-  index do 
+
+  index :download_links => [:pdf] do
     column :id
     column :username
+    default_actions
   end
 
 
   form do |f|
-    f.inputs do 
+    f.inputs do
       f.input :username
       f.input :email
     end
